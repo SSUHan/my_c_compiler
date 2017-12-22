ee790156dca95ba0a44d78655c304cef43e74409
이준수 <ljs93kr@gmail.com>
2017-12-22 22:33:55 +0900

    debug log

--- sementic/main.c
+++ sementic/main.c
@@ -55,12 +55,12 @@ void main(int argc, char *argv[])
 	semantic_analysis(root);
 	if (semantic_err) 
 		exit(1);
-	
+
 	//print_sem_ast(root);
-	/*
+	
 	printf("start code generation\n");
 	code_generation(root);
 	printf("end code generation\n");
-	*/
+
 	exit(0);
 }
